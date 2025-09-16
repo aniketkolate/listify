36aa0ef980cb62abb09b5899c5b3886d11609ef0
vaibhavkate7 <katevaibhav417@gmail.com>
2025-09-16 16:05:24 +0530

    feat: implementation edit task card functionality

--- src/app/dashboard/components/task-card/task-card.component.ts
+++ src/app/dashboard/components/task-card/task-card.component.ts
@@ -1,20 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgClass, CommonModule } from '@angular/common';
 import { Task, } from '../../models/common.model';
+import { AddTaskComponent } from "../add-task/add-task.component";
 
 
 @Component({
   selector: 'lst-task-card',
   standalone: true,
-  imports: [CommonModule, NgClass],
+  imports: [CommonModule, NgClass, AddTaskComponent],
   templateUrl: './task-card.component.html',
   styleUrls: ['./task-card.component.scss']
 })
 export class TaskCardComponent {
-  [x: string]: any;
   @Output() delete = new EventEmitter();
   @Output() edit = new EventEmitter();
-  @Input() disabled = true;
+  @Output() editTaskEvent = new EventEmitter();
+  @Input() disabled = false;
   @Input() task: Task = {
     id: 0,
     title: '',
@@ -22,8 +23,28 @@ export class TaskCardComponent {
     status: 'TODO'
   };
 
+  isEditable: boolean = false;
+  taskService: any;
+
   deleteTask(taskId: number) {
     this.delete.emit(taskId);
+  }
+  editTask(updatedTask: Task) {
+    this.isEditable = false;
+    this.editTaskEvent.emit(updatedTask);
+  }
+
+  editTaskCard(updatedTask: Task) {
+    this.taskService.editTask(updatedTask);
+    this.getAllTask();
+  }
+
+  getAllTask() {
+    throw new Error('Method not implemented.');
+  }
+
+  isEditTaskEnable() {
+    this.isEditable = true;
   }
 }
 

01a50d51daf9ee76d752e691ba4d01a06b05098e
aniketkolate <aniketkolte18@gmail.com>
2025-09-28 18:08:31 +0530

    refactor: remove header component from lst component and implement load userservice logic on OnInit

--- src/app/lst.component.ts
+++ src/app/lst.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./dashboard/components/header/header.component";
 import { FormsModule } from '@angular/forms';
+import { UserService } from './core/services/user/user.service';
 
 @Component({
   selector: 'lst-root',
-  imports: [RouterOutlet, HeaderComponent, FormsModule],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './lst.component.html',
   styleUrl: './lst.component.scss'
 })
-export class LstComponent {
+export class LstComponent implements OnInit {
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.loadUserFromStorage();
+  }
 
 }
